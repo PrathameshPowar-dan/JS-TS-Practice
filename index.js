af1752ed4fb79446da4fe5064f62b78e7d97e8e4
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-02-26 23:35:42 +0530

    Calc

--- index.js
+++ index.js
@@ -189,16 +189,34 @@ const ProductArr = [{ name: "table", productName: "Ultra" }, { name: "drop", pro
 // 12. The Token Manager:
 //     You are developing a user authentication system, and you need to manage user authentication tokens. Implement a function named setAuthToken that takes an authentication token and sets it in localStorage with an expiration time.
 
-function setAuthToken(token){
+function setAuthToken(token) {
     const expiryTime = Date.now() + 10000;
 
     const data = {
         token,
         expiry: expiryTime
     }
-    localStorage.setItem("AuthToken",JSON.stringify(data));
+    localStorage.setItem("AuthToken", JSON.stringify(data));
 
     return data
 }
 
 // console.log(setAuthToken("token"));
+
+
+// 13. The Shopping Cart Totalizer:
+//     You are working on an e-commerce website, and you need to calculate the total cost of items in the shopping cart. Implement a function named calculateTotal that takes an array of products with prices and quantities and returns the total cost.
+
+function calculateTotal(arr){
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) { 
+            total += arr[i].price * arr[i].quantity
+    }
+
+    return total;
+}
+
+const pro = [{name:"Banana",price:100,quantity:2},{name:"Apple",price:10,quantity:2},{name:"Orange",price:40,quantity:2}]
+
+// console.log("Total Price",calculateTotal(pro));
+
